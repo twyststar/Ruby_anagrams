f49dfddad03db22cdcf119214b600300eb3bbb65
Starlynne <lewis.start0905@gmail.com>
2017-04-21 09:29:28 -0700

    Add logic and spec for checking words with different captalization. Pass spec.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,8 @@
 class String
   define_method(:anagram?) do |word|
-    word1 = self.split("").to_a
-    word2 = word.split("").permutation(word1.length).to_a
+    word1 = self
+    word1 = word1.downcase().split("").to_a
+    word2 = word.downcase().split("").permutation(word1.length).to_a
     if word2.include?(word1)
       "That's an anagram."
     else
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,7 +2,10 @@ require('rspec')
 require('anagram')
 
 describe('String#anagram') do
-  it('will compare two inputs and return if they are anagrams') do
+  it('will compare two inputs of the same case and return if they are anagrams') do
     expect('cat'.anagram?('act')).to eq("That's an anagram.")
+  end
+  it('will compare two inputs with letters in different case and return if they are anagrams') do
+    expect('Cat'.anagram?('acT')).to eq("That's an anagram.")
   end
 end
