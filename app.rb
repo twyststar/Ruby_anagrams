bf67af9da7983fbae02c6c425df3a905b41de0bc
Starlynne <lewis.start0905@gmail.com>
2017-04-21 11:51:38 -0700

    Add and pass first integration spec.

--- app.rb
+++ app.rb
@@ -7,3 +7,8 @@ also_reload('lib/**/*.rb')
 get('/') do
   erb(:index)
 end
+
+post('/form') do
+  @answer = params.fetch('word1').anagram?(params.fetch('word2'))
+  erb(:index)
+end
--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -3,11 +3,12 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# describe('anagram path', {:type => :feature}) do
-#   it (') do
-#   visit('/')
-#   fill_in('', :with => '')
-#   click_button('')
-#   expect(page).to have_content("")
-#   end
-# end
+describe('anagram path', {:type => :feature}) do
+  it ('will return affirmative answer if input words are anagram of eachother') do
+    visit('/')
+    fill_in('word1', :with => 'cat')
+    fill_in('word2', :with => 'act')
+    click_button('????')
+    expect(page).to have_content("That\'s an anagram.")
+  end
+end
