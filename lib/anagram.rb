2258af62ead0141e126eff35bab1c707620a0f4f
Starlynne <lewis.start0905@gmail.com>
2017-04-21 16:47:36 -0700

    Add meow and hello methods

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,8 @@
+
 class String
   define_method(:anagram?) do |word|
-    word1 = self
-    word1 = word1.downcase().gsub(/[\W\d]/, '').split("").reject{ |e|  e == " " }
-    word2 = word.downcase().gsub(/[\W\d]/, '').split("").reject{ |e|  e == " " }
+    word1 = self.prep_words()
+    word2 = word.prep_words()
     vowels = ['a', 'e', 'i', 'o', 'u']
     both = word1 & word2
     if (word1 & vowels).empty?() || (word2 & vowels).empty?()
@@ -12,8 +12,6 @@ class String
     else
       if word1.length != word2.length && both.length > 0
         "Not an anagram, but the letters '" + both.join() + "' are present in both fields."
-      elsif word1.length != word2.length
-        "Not an anagram."
       elsif word2.sort() == word1.sort()
         "That's an anagram."
       else
@@ -37,8 +35,28 @@ class String
 end
 
 
-# class String
-#   define_method(:generate) do
-#
-#   end
-# end
+class String
+  define_method(:prep_words) do
+    word1 = self
+    word1 = word1.downcase().gsub(/[\W\d]/, '').split("").reject{ |e|  e == " " }
+  end
+end
+
+class String
+  define_method(:kitty) do
+    if (self == 'kitty') || (self =='cat') || (self =='meow') || (self =='purr')
+     "Meow! "
+    else
+      " "
+    end
+  end
+end
+class String
+  define_method(:hello) do
+    if (self == 'hello') || (self == 'hi') || (self =='aloha')
+     "Hello to you! "
+    else
+     " "
+    end
+  end
+end
