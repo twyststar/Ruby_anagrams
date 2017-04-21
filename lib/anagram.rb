474a1561df42c68f1779895b60923a4bea614b12
Starlynne <lewis.start0905@gmail.com>
2017-04-21 10:23:39 -0700

    Add logic and spec to return negative if input lengths do not match, pass spec.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,10 +3,17 @@ class String
     word1 = self
     word1 = word1.downcase().split("")
     word2 = word.downcase().split("")
-    if word2.sort() == word1.sort()
-      "That's an anagram."
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    if (word1 & vowels).empty?() == true || (word2 & vowels).empty?()
+      "Please enter a valid word."
     else
-      "Not an anagram."
+      if word1.length != word2.length
+        "Not an anagram."
+      elsif word2.sort() == word1.sort()
+        "That's an anagram."
+      else
+        "Not an anagram."
+      end
     end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,6 +2,9 @@ require('rspec')
 require('anagram')
 
 describe('String#anagram') do
+  it('will return not an anagram if input lengths are not equal') do
+    expect('catt'.anagram?('act')).to eq("Not an anagram.")
+  end
   it('will compare two input words of the same case and return if they are anagrams') do
     expect('cat'.anagram?('act')).to eq("That's an anagram.")
   end
@@ -10,5 +13,8 @@ describe('String#anagram') do
   end
   it('will compare two inputs longer than one word and return if they are anagrams') do
     expect('Cat is crazy'.anagram?('act racy siz')).to eq("That's an anagram.")
+  end
+  it('will ask for correct input if either input does not contain vowels') do
+    expect('Ct s crzy'.anagram?('act racy siz')).to eq("Please enter a valid word.")
   end
 end
